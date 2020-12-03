e53110a5f687d3c5705c83c6cb7a657f040093cd
Estela <estelajimero@users.noreply.github.com>
2020-12-03 12:05:50 +0100

    first commit

---
+++ Day 1/day1.js
@@ -0,0 +1,26 @@
+// DAY 1 ADVENT CODE
+let fs = require('fs');
+let text = fs.readFileSync('./Day 1.txt').toString('utf-8');
+
+let expenseReport = text.split('\r\n');
+
+for (i = 0; i < expenseReport.length; i++) {
+    let value = parseInt(expenseReport[i]);
+
+    for(j = 0; j < expenseReport.length; j++) {
+        let secondValue = parseInt(expenseReport[j]);
+
+        for(z = 0; z < expenseReport.length; z++) {
+            let thirdValue = parseInt(expenseReport[z]);
+
+            if(value != secondValue && value != thirdValue) {
+                if(value + secondValue + thirdValue === 2020) {
+                    console.log(`Three entries that sum 2020 are ${value}, ${secondValue} & ${thirdValue}, multiplying them you get ${value * secondValue * thirdValue}`);
+                    
+                    return;
+                }
+            }
+        }
+    }
+}
+
---
+++ Day 2/day2.js
@@ -0,0 +1,51 @@
+let fs = require('fs');
+let text = fs.readFileSync('./day2.txt').toString('utf-8');
+
+let passwordInput = text.split('\r\n');
+
+function passwords (maxnumber, minnumber, letter, pass) {
+    this.maxnumber = maxnumber
+    this.minnumber = minnumber
+    this.letter = letter
+    this.pass = pass
+}
+
+let passwordList = [];
+
+passwordInput.forEach(function(element, index, array) {
+    let passwordMin = element.substring(0, element.indexOf('-'));
+    let passwordMax = element.substring(element.indexOf('-') + 1, element.indexOf(' '));
+    let letter = element.substring(element.indexOf(' ') + 1, element.indexOf(':'));
+    let pass = element.substring(element.indexOf(': ') + 2, element.length);
+
+    let passwordObject = new passwords(passwordMax, passwordMin, letter, pass);
+
+    passwordList.push(passwordObject);
+});
+
+// PART ONE 
+let count = 0;
+
+passwordList.forEach(function(element, index, array) {
+    let charCount = element.pass.split(element.letter).length - 1;
+
+    if(charCount >= element.minnumber && charCount <= element.maxnumber) { 
+        count++;
+    }
+})
+
+// //PART TWO
+// let count = 0;
+
+// passwordList.forEach(function(element, index, array) {
+//     let firstPosition = element.pass[parseInt(element.minnumber) - 1];
+//     let secondPosition = element.pass[parseInt(element.maxnumber) - 1];
+
+//     if(element.letter === firstPosition && element.letter != secondPosition) {
+//         count++;
+//     } else if (element.letter === secondPosition && element.letter != firstPosition) {
+//         count++;
+//     }
+// });
+
+console.log(`There are ${count} valid passwords`);
